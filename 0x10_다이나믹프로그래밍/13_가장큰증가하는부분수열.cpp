e98edb2154d25e5459d9f2c86744526b4c9985f6
SoinDosa <rlatjsgnl519@gmail.com>
2023-08-09 08:47:27 +0900

    0x10_13 풀이

---
+++ 0x10_다이나믹프로그래밍/13_가장큰증가하는부분수열.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int A[1001];
+int dp[1001];
+
+int main()
+{
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> A[i];
+		dp[i] = A[i];
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			if (A[j] < A[i])
+			{
+				dp[i] = max(dp[i], dp[j] + A[i]);
+			}
+		}
+	}
+
+	cout << *max_element(dp, dp + N);
+}
